6f5c2ac321c0ea0a5165bc90b7d0f4d6c136cb4c
Mathieu Giguere <mathieu.giguere@umanomedical.com>
2021-02-02 11:07:14 -0500

    Reduce verbosity of ccache msg

--- ccache.cmake
+++ ccache.cmake
@@ -1,7 +1,7 @@
 find_program(CCACHE ccache)
 cmake_dependent_option(CCACHE_ACTIVATE "Use CCache to potential speed up re-compile time" ON "CCACHE" OFF)
 if(CCACHE_ACTIVATE)
-  message("CCACHE : In use")
+  message(VERBOSE "CCACHE : In use")
   set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
   set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE})
 
@@ -23,7 +23,7 @@ if(CCACHE_ACTIVATE)
   endif()
 
 elseif(CCACHE)
-  message("CCACHE Not Activate; Was found but requested to not be used")
+  message(VERBOSE "CCACHE Not Activate; Was found but requested to not be used")
 else ()
-  message("CCACHE not found; cannot use")
+  message(VERBOSE "CCACHE not found; cannot use")
 endif()
